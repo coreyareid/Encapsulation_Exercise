d8f0ee6eacb5a8d8f184039eb66ffbc495943567
coreyareid <coreyareid@bellsouth.net>
2022-07-14 15:24:52 -0500

    Completed challange

---
+++ Encapsulation_Exercise/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Encapsulation_Exercise
+{
+    public class BankAccount
+    {
+        // Default Contructor
+        public BankAccount()
+        {
+        }
+
+        // Fields
+        private double balance = 0;
+
+        // Properties
+        // public double Balance { get; set; }
+
+
+        // Methods
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+    }
+}
+
--- Encapsulation_Exercise/Program.cs
+++ Encapsulation_Exercise/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Encapsulation_Exercise;
 
+BankAccount account = new BankAccount();
+
+
+
+Console.WriteLine("How much would you like to deposit?");
+var amountToDeposit = double.Parse(Console.ReadLine());
+
+account.Deposit(amountToDeposit);
+
+Console.WriteLine($"Thanks you! Your balance is now {account.GetBalance()}");
+
+Console.WriteLine("\nHow much would you like to withdraw?");
+
+
+var amountToWithdraw = double.Parse(Console.ReadLine());
+account.Withdraw(amountToWithdraw);
+
+Console.WriteLine($"Thanks you! Your balance is now {account.GetBalance()}");
